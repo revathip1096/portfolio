2c0a857ba75c0ea2732cc27f6bfce6835326d9f9
Revathi <Revathi092001@gmail.com>
2023-08-21 19:02:23 +0530

    3rd commit

--- src/components/Projectlist.jsx
+++ src/components/Projectlist.jsx
@@ -1,6 +1,6 @@
 import ProjectCard from "../components/Projectcard";
 import { Container, Row, Col } from "react-bootstrap";
-import adminpanel from '/src/assets/images/adminpanel.jpg'
+
 import todolist from "/src/assets/images/todolist.jpg";
 import "../pages/style.css";
 
@@ -11,15 +11,7 @@ function Projectlist() {
       <Container fluid className="project-section">
         <Container>
           <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-            <Col md={4} className="project-card">
-              <ProjectCard
-                imgPath={adminpanel}
-                isBlog={false}
-                title="Dashboard Panel"
-                description="User Admin Dashboard created with React js, Material UI, Nivo Charts, Formik, Yup, FullCalendar, and Data Grid to build this entire application. This application consists of Light and Dark Mode, four different Charts, three different Data Table Pages, FAQ Page, Form Page, and Calendar Integration."
-                ghLink="https://rahul-dashboard-pannel.netlify.app"
-              />
-            </Col>
+            
 
 
             <Col md={4} className="project-card">
